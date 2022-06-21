1f0887bab4719c823360488bb713e1adf85f153f
selvam <selvamplkm111@gmail.com>
2022-06-21 21:03:16 +0530

    add-folder

---
+++ script.js
@@ -0,0 +1,14 @@
+let x;
+x = 78;
+if (x >= 90) {
+    console.log(" you are A grade");
+}
+else if (x >= 75) {
+    console.log(" you are B grade");
+}
+else if (x >= 50) {
+    console.log(" you are c grade");
+}
+else {
+    console.log(" you are fail");
+}
